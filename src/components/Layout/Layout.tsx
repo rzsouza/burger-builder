3b5a6e169c37cff629eac9005fee2be890a50686
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2020-08-20 11:03:43 +1200

    Added toolbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.module.scss';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout: React.FC = ({ children }) => (
   <Fragment>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{children}</main>
   </Fragment>
 );
