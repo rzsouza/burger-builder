e873674d52f97d24f98fb9c32753263dafa6a7a8
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2020-08-26 15:35:22 +1200

    Added hook for http error handling

--- src/hoc/withErrorHandler/withErrorHandler.tsx
+++ src/hoc/withErrorHandler/withErrorHandler.tsx
@@ -1,50 +1,18 @@
-import React, {
-  ComponentType,
-  Fragment,
-  PropsWithChildren,
-  useEffect,
-  useState,
-} from 'react';
+import React, { ComponentType, Fragment, PropsWithChildren } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import { AxiosInstance } from 'axios';
-
-type ErrorType = string | undefined | null;
+import useHttpErrorHandler from '../../hooks/useHttpErrorHandler';
 
 function withErrorHandler(
   WrappedComponent: ComponentType,
   axios: AxiosInstance
 ) {
   return (props: PropsWithChildren<any>) => {
-    const [error, setError] = useState<ErrorType>(null);
-
-    const reqInterceptor = axios.interceptors.request.use((req) => {
-      setError(null);
-      console.log('err null');
-      return req;
-    });
-
-    const resInterceptor = axios.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        if (err) {
-          console.log(err.message);
-          setError(err.message);
-          console.log(!!err.message);
-        }
-      }
-    );
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(resInterceptor);
-      };
-    }, [reqInterceptor, resInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      console.log('err null reset');
-      setError(null);
-    };
+    // @ts-ignore
+    const [error, errorConfirmedHandler]: [
+      string | undefined | null,
+      () => void | undefined
+    ] = useHttpErrorHandler(axios);
 
     return (
       <Fragment>
---
+++ src/hooks/useHttpErrorHandler.ts
@@ -0,0 +1,40 @@
+import { AxiosInstance } from 'axios';
+import { useEffect, useState } from 'react';
+
+type ErrorType = string | undefined | null;
+
+export default (axios: AxiosInstance) => {
+  const [error, setError] = useState<ErrorType>();
+
+  const reqInterceptor = axios.interceptors.request.use((req) => {
+    setError(null);
+    console.log('err null');
+    return req;
+  });
+
+  const resInterceptor = axios.interceptors.response.use(
+    (res) => res,
+    (err) => {
+      if (err) {
+        console.log(err.message);
+        setError(err.message);
+        console.log(!!err.message);
+      }
+    }
+  );
+
+  useEffect(() => {
+    return () => {
+      axios.interceptors.request.eject(reqInterceptor);
+      axios.interceptors.response.eject(resInterceptor);
+    };
+    // eslint-disable-next-line
+  }, [reqInterceptor, resInterceptor]);
+
+  const errorConfirmedHandler = () => {
+    console.log('err null reset');
+    setError(null);
+  };
+
+  return [error, errorConfirmedHandler];
+};
